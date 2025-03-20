a79a8841efc38b88c137158ad88eec91d33f896c
orgdevendra <devcloud010.gmail.com>
2025-03-20 16:17:06 +0530

    all files

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import logging
 import azure.functions as func
 from azure.messaging.webpubsubservice import WebPubSubServiceClient
@@ -11,3 +12,15 @@ def main(msg: func.ServiceBusMessage):
     # Send message to Web PubSub
     message = msg.get_body().decode('utf-8')
     webpubsub_client.send_to_all(message)
+=======
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.service_bus_topic_trigger(arg_name="azservicebus", subscription_name="wind_topic", topic_name="wind_topic",
+                               connection="servicebustopic0318_SERVICEBUS") 
+def servicebus_topic_trigger(azservicebus: func.ServiceBusMessage):
+    logging.info('Python ServiceBus Topic trigger processed a message: %s',
+                azservicebus.get_body().decode('utf-8'))
+>>>>>>> c584775 (initial functionapp commit)
